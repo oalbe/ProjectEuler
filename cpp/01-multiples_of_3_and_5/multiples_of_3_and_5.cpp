199913b663d6a96f8814f59a07bdf4426e560160
Alberto <alberto.ognibene@gmail.com>
2016-01-23 20:00:05 +0100

    (cpp) multiples_of_3_and_5.cpp - Done.

--- cpp/01-multiples_of_3_and_5/multiples_of_3_and_5.cpp
+++ cpp/01-multiples_of_3_and_5/multiples_of_3_and_5.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv) {
     sum_three = sum_five = 0;
     for (size_t i = 0; i < 1000; ++i) {
         if ((i % 3) == 0) {
-            std::cout << "3: " << i << std::endl;
+            // std::cout << "3: " << i << std::endl;
             sum_three += i;
         } else if ((i % 5) == 0) {
             std::cout << "5: " << i << std::endl;
