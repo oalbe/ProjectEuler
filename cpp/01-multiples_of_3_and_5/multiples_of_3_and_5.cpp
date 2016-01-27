54ffaf48ee0072b4ba7e731d3454790a4c18a6d2
Alberto <alberto.ognibene@gmail.com>
2016-01-27 20:24:19 +0100

    (cpp) multiples_of_3_and_5.cpp - Removed the unused vector header.

--- cpp/01-multiples_of_3_and_5/multiples_of_3_and_5.cpp
+++ cpp/01-multiples_of_3_and_5/multiples_of_3_and_5.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <vector>
 
 int main(int argc, char ** argv) {
-    unsigned int sum_three, sum_five, total;
+    unsigned int sum_three, sum_five;
 
     sum_three = sum_five = 0;
     for (size_t i = 0; i < 1000; ++i) {
@@ -15,7 +14,7 @@ int main(int argc, char ** argv) {
         }
     }
 
-    total = sum_three + sum_five;
+    unsigned int total = sum_three + sum_five;
 
     std::cout << "Total: " << total << std::endl;
 
