2f4df4bcb8485d4d9a1b6942b4b57dfe44112067
Alberto <alberto.ognibene@gmail.com>
2016-02-02 15:06:00 +0100

    (cpp) special_pythagorean_triplet.cpp - Done.

---
+++ cpp/09-special_pythagorean_triplet/special_pythagorean_triplet.cpp
@@ -0,0 +1,16 @@
+// https://stackoverflow.com/questions/575117/generating-unique-ordered-pythagorean-triplets
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    int sum, a, b, c;
+
+    sum = a = b = c = 0;
+
+    while (sum != 1000) {
+
+
+        sum = a + b + c;
+    }
+    
+    return 0;
+}
